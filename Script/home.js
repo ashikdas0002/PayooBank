9d4893164d546ee1e3345ba76452deb554539d07
ashikdas0002 <ashikdas0002@gmail.com>
2025-03-22 01:37:42 +0600

    first commit

--- Script/home.js
+++ Script/home.js
@@ -1,3 +1,12 @@
 document.getElementById('logout-btn').addEventListener('click', function () {
     window.location.href = 'login.html'
 })
+
+
+document.getElementById('addMoney').addEventListener('click', function (event) {
+    event.preventDefault();
+    const addMoneyInput = document.getElementById('add-money-field').value;
+    const pinNumberInput = document.getElementById('pin-field').value;
+    console.log(addMoneyInput, pinNumberInput);
+
+}); 
